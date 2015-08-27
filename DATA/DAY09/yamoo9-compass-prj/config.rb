25f65fd0d4f747f9e0d8bca43c22e47b17f31ea5
Jee Hoon <yamoo9@naver.com>
2015-08-27 19:19:08 +0900

    10일차 수업 Compass 프로젝트 준비

--- DATA/DAY09/yamoo9-compass-prj/config.rb
+++ DATA/DAY09/yamoo9-compass-prj/config.rb
@@ -3,10 +3,10 @@ require 'bourbon'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
+http_path       = "/"
+css_dir         = "css"
+sass_dir        = "sass"
+images_dir      = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
